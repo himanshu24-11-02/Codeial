313da2562ed93b8dfb7fa904f831ede34ed48391
Himanshu <uietcse2024himanshusharma@kuk.ac.in>
2023-10-20 11:35:01 +0530

    add a controller and a router for home

---
+++ routes/index.js
@@ -0,0 +1,10 @@
+// this file will handle all the routes 
+const express = require("express");
+const router = express.Router();
+const homeController  = require('../controllers/home_controller')
+
+
+router.get("/", homeController.home)
+
+
+module.exports = router;
